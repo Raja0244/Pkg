bcc88cbd8fc9b3df9378030ce3fe1b94b686f1b8
Raja0244 <rajendra.madana@bitsinglass.com>
2025-07-23 15:04:15 +0530

    Deleted application

--- UsrRealty/Schemas/UsrTestRealty_Detail/UsrTestRealty_Detail.js
+++
@@ -1,8 +0,0 @@
-define('UsrTestRealty_Detail', [], function () {
-	return {
-		entitySchemaName: 'UsrRealty',
-		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
-		methods: {},
-	};
-});
